fcb46927b4a357867ac6b1542af5524ec4fb6924
Linney Dew <ldew@hendersondatasolutions.com>
2024-09-19 15:36:11 -0400

    add new url to HDS_Config.js and add reference in sign-in.html

--- Scripts/HDS_Config.js
+++ Scripts/HDS_Config.js
@@ -1,5 +1,6 @@
 ﻿localStorage.setItem("apiAuthenticate", "https://localhost:7002/api/authenticate");
 localStorage.setItem("apiTwoFactorVerify", "https://localhost:7002/api/TwoFactorVerify");
 localStorage.setItem("apiInvoicesGetCustomers", "https://localhost:7002/Invoices/GetCustomers");
+localStorage.setItem("RedirUri", "https://localhost:7002/api/Authresponse");
 
 localStorage.setItem("apiSendEmail", "https://uat1.hendersondatasolutions.com:8093/api/PostMessage");
