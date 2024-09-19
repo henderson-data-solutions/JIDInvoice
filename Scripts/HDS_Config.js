180c5aaa308a58fa67013f0a16c7efa36beb0949
Linney Dew <ldew@hendersondatasolutions.com>
2024-09-19 07:17:46 -0400

    Task 42 - HDS Auth changes

--- Scripts/HDS_Config.js
+++ Scripts/HDS_Config.js
@@ -1,8 +1,5 @@
-﻿localStorage.setItem("client_id", "web-client");
-localStorage.setItem("client_secret", "901564A5-E7FE-42CB-B10D-61EF6A8F3654");
-
-localStorage.setItem("apiAuthenticate", "https://localhost:7002/Accounts/MobileAuthenticate");
-localStorage.setItem("apiTwoFactorVerify", "https://localhost:7002/Accounts/TwoFactorVerify");
+﻿localStorage.setItem("apiAuthenticate", "https://localhost:7002/api/authenticate");
+localStorage.setItem("apiTwoFactorVerify", "https://localhost:7002/api/TwoFactorVerify");
 localStorage.setItem("apiInvoicesGetCustomers", "https://localhost:7002/Invoices/GetCustomers");
 
 localStorage.setItem("apiSendEmail", "https://uat1.hendersondatasolutions.com:8093/api/PostMessage");
