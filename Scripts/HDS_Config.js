6cc9ef1b0d2c2cb2721abda98ba9091470c40d38
Linney Dew <ldew@hendersondatasolutions.com>
2024-09-20 09:34:01 -0400

    add sign-in and sign-up pages

--- Scripts/HDS_Config.js
+++ Scripts/HDS_Config.js
@@ -2,5 +2,6 @@
 localStorage.setItem("apiTwoFactorVerify", "https://localhost:7002/api/TwoFactorVerify");
 localStorage.setItem("apiInvoicesGetCustomers", "https://localhost:7002/Invoices/GetCustomers");
 localStorage.setItem("RedirUri", "https://localhost:7002/api/Authresponse");
+localStorage.setItem("apiRegisterUser", "https://localhost:7002/Users/Register");
 
 localStorage.setItem("apiSendEmail", "https://uat1.hendersondatasolutions.com:8093/api/PostMessage");
