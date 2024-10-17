3b5867181c01b0e5c64e42a79fa42fa10f4a37cf
Linney Dew <ldew@hendersondatasolutions.com>
2024-10-17 15:48:19 -0400

    add hds-invoice.html

--- Scripts/HDS_Config.js
+++ Scripts/HDS_Config.js
@@ -1,6 +1,7 @@
 ﻿localStorage.setItem("apiAuthenticate", "https://localhost:7002/api/authenticate");
 localStorage.setItem("apiTwoFactorVerify", "https://localhost:7002/api/TwoFactorVerify");
 localStorage.setItem("apiInvoicesGetCustomers", "https://localhost:7002/Invoices/GetCustomers");
+localStorage.setItem("apiGetInvoiceByInvoiceNo", "https://localhost:7002/Invoices/GetInvoiceByInvoiceNo");
 localStorage.setItem("RedirUri", "https://localhost:7002/api/Authresponse");
 localStorage.setItem("apiRegisterUser", "https://localhost:7002/Users/Register");
 
