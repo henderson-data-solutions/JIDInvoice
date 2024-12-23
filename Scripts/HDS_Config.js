3108e81aa8d0ee55ec882d1c8728475826a57791
Linney Dew <ldew@hendersondatasolutions.com>
2024-12-23 14:35:54 -0500

    email two factor verification working

--- Scripts/HDS_Config.js
+++ Scripts/HDS_Config.js
@@ -1,8 +1,8 @@
 ﻿localStorage.setItem("apiAuthenticate", "https://localhost:7282/api/authentication/Authenticate");
 localStorage.setItem("apiTwoFactorVerify", "https://localhost:7282/api/authentication/TwoFactorVerify");
 localStorage.setItem("apiRequestTwoFactorCode", "https://localhost:7282/api/authentication/RequestTwoFactor"); 
-localStorage.setItem("apiInvoicesGetCustomers", "https://localhost:7282/Invoices/GetCustomers");
-localStorage.setItem("apiGetInvoiceByInvoiceNo", "https://localhost:7282/Invoices/GetInvoiceByInvoiceNo");
+localStorage.setItem("apiInvoicesGetCustomers", "https://localhost:7282/api/invoices/GetCustomers");
+localStorage.setItem("apiGetInvoiceByInvoiceNo", "https://localhost:7282/api/invoices/GetInvoiceByInvoiceNo");
 localStorage.setItem("apiCheckUserAuthentication", "https://localhost:7282/Users/CheckAuthenticationStatus");
 
 localStorage.setItem("RedirUri", "https://localhost:7282/api/Authresponse");
